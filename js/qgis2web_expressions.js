8f606f8cb6bef837ff0b00b1d48df67293c30cfb
juan pablo gomez <jugomezu@unal.edu.co>
2026-05-26 08:29:49 -0500

    Replace site with 15 con 106 map

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -754,17 +754,3 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
-
-
-
-function exp_ActuacionesEstrategicas_4rule0_eval_expression(context) {
-    // Nombre = 'AE Fontibon'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return (feature.properties['Nombre']  == 'AE Fontibon');
-    } else {
-        return (feature['Nombre']  == 'AE Fontibon');
-    }
-}
